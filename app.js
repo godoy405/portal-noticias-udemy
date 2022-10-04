7e6e2c77ed12fae7d760830ecc15f63d9d656506
Gilberto Neri Godoy <gilbertonerigodoy@gmail.com>
2022-10-04 19:27:17 -0300

    Criação de rotas

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var express = require('express')
-var app = express()
+var app = require('./config/server')
 
-app.set('view engine', 'ejs')
+var rotaNoticias = require('./app/router/noticias')(app)
 
-app.get('/', function(req, res) {
-    res.render("home/index")
-})
+var rotaHome = require('./app/router/home')(app)
 
-app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render("admin/form_add_noticia")
-})
-
-app.get('/noticias', function(req, res) {
-    res.render("noticias/noticias")
-})
+var rotaFormInclusaoNoticia = require('./app/router/formulario_inclusao_noticia')(app)
 
 app.listen(3000, function() {
-    console.log("Servidor rodando com express")
+    console.log("Servidor ON")
 })
